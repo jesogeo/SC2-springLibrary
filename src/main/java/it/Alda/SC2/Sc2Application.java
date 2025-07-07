f69f87a7272fc53a343b040487e0df85159bc00c
Alda Drinalda H <drinalda@yahoo.com>
2025-07-07 23:26:48 +0200

    first commit

---
+++ src/main/java/it/Alda/SC2/Sc2Application.java
@@ -0,0 +1,13 @@
+package it.Alda.SC2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sc2Application.class, args);
+	}
+
+}
