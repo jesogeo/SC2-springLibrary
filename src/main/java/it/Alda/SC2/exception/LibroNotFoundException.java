8a73f7972199c4ee4c6f931144b7443b6d8fcce4
unozer <40232537+unozer@users.noreply.github.com>
2025-07-08 19:16:52 +0200

    Tolto advisor. Va fatta una classe a parte (es. Global Exception Handler) che funge da Advisor.

--- src/main/java/it/Alda/SC2/exception/LibroNotFoundException.java
+++ src/main/java/it/Alda/SC2/exception/LibroNotFoundException.java
@@ -1,12 +1,9 @@
 package it.Alda.SC2.exception;
 
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
 
-@ControllerAdvice
 public class LibroNotFoundException extends RuntimeException {
     public LibroNotFoundException(String message) {
         super(message);
     }
-
 }
